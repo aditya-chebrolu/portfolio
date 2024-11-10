f24a85888b2e7522a39e8e85cab1aa5379b387b6
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-11-11 00:44:37 +0530

    code changes

--- src/app/(home)/components/contact/index.tsx
+++ src/app/(home)/components/contact/index.tsx
@@ -1,25 +1,7 @@
 "use client";
-import In from "@svgs/social/in.svg";
-import Github from "@svgs/social/github.svg";
-import Mail from "@svgs/social/mail.svg";
-import { Dock, DockIcon } from "@/components/ui/dock";
 
 const Contact = () => {
-  return (
-    <div className="fixed bottom-4 left-0 right-0 z-50">
-      <Dock direction="middle" className="rounded-full p-0">
-        <DockIcon className="">
-          <In className="h-[15px] w-[15px] fill-black dark:fill-white md:h-[15px] md:w-[15px]" />
-        </DockIcon>
-        <DockIcon>
-          <Github className="h-[15px] w-[15px] fill-black dark:fill-white md:h-[15px] md:w-[15px]" />
-        </DockIcon>
-        <DockIcon>
-          <Mail className="h-[15px] w-[15px] fill-black dark:fill-white md:h-[15px] md:w-[15px]" />
-        </DockIcon>
-      </Dock>
-    </div>
-  );
+  return <div className="fixed bottom-4 left-0 right-0 z-50"></div>;
   // return (
   //   <div className="flex flex-col gap-4">
   //     <h2 className="text-center text-4xl/tight font-bold">Contact Me</h2>

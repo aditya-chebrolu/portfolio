e7b67d090f5c3aa7cc435da3ea97e0fc42eb6075
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-11-12 00:20:45 +0530

    code changes

--- src/components/custom/page-wrapper.tsx
+++ src/components/custom/page-wrapper.tsx
@@ -5,7 +5,7 @@ const PageWrapper = ({ children, className }: { children: React.ReactNode; class
   return (
     <main
       className={cn(
-        "my-[var(--block-spacing)] grid grid-cols-[20px_1fr_20px] grid-rows-[auto] gap-y-10 md:grid-cols-[1fr_600px_1fr] md:gap-y-12 [&>*:not(.col-span-full)]:col-[2/3]",
+        "my-[var(--block-spacing)] grid grid-cols-[25px_1fr_25px] grid-rows-[auto] gap-y-10 md:grid-cols-[1fr_600px_1fr] md:gap-y-12 [&>*:not(.col-span-full)]:col-[2/3]",
         className
       )}
     >
